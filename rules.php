47f2887b1310ad31bcb634ab37047a6970cff022
GitHugop <ori.ginalhugop@gmail.com>
2017-11-30 23:37:41 +0100

    Create rules.php

---
+++ rules.php
@@ -0,0 +1,109 @@
+<?php
+
+require("php/include.php");
+$page = new Page();
+$page->showHeader('Rules');
+?>
+
+<link href="css/rules.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<link href="css/tos.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<style>.parallax-container .parallax img {opacity:100;}</style>
+</head>
+<body class="black">
+
+<?php
+$page->showNav();
+?>
+
+<div class="container">
+  <div class="section">
+    <div class="row">
+      <div class="col s12">
+          <h2 class="white-text center">International Video Game and Anime Syndicate (IVGAS) Discord Rules</h2>
+      <p class="white-text center">The International Video Game and Anime Syndicate or I.V.G.A.S for short, is a group of people dedicated to Videogames and Anime as a primary topic focus.
+The overall objective for the group is to relax, hang out, and make friends using the Ani
+me and Video Game Focus.
+</p>
+          <h3 class="white-text">Rules:</h3>
+    <ol class="white-text">
+      <li>Common Sense applies here. If you refuse to use it, do not come here. Period.</li>
+      <li>There is a Discord ToS, please make sure you follow them at all times so that I.V.G.A.S can be around for everyone to enjoy.</li>
+      <li>Do not come into I.V.G.A.S’s discord bringing chaos and drama with you from DMs or other servers. It's not welcomed here and will result in "any" punishment 100chilly or the Committee sees fit. We don't want your drama or bullshit. Period.
+</li>
+      <li>There are people who can easily take offense, so please be cautious think before you start a discussion that includes: religious/political beliefs, other countries and territories, ETC. 
+</li>
+      <li>Please try to keep discussions PG-13 or lower outside of the NSFW-designated rooms.</li>
+      <li>Please refrain from posting any NSFW(18+) content outside of the designated channels. Thank You!</li>
+      <li>Drama/Spam/"Being Cancerous"/disrupting the ecosystem of this server is forbidden. If you are causing issues with people you are not acquainted/familiar with are are asked to stop, please stop what you're doing when and if someone tells you to stop. We are a drama-free server. Posting random text such as “vkjadvbvfjvbhdf” and emotes/emojis that contribute nothing and falls under spamming offenses.
+</li>
+      <li>Please be respectful of others and their lifestyles, preferences, beliefs or choices. We are all here to hang out and make friends.</li>
+      <li>Please be wary about your microphone usage. If you have too much background noise and we ask you to either fix or go push to talk, please do so or we will revoke your voice activity permissions.</li>
+      <li>Promoting others to break the Discord ToS as well as our server rules is forbidden. Doing this will not help you or your “cause”. Ban evading falls under this and will result in more administrative action.</li>
+      <li>No one here gets special treatment from 100chilly or the staff. Everyone is assigned an administrator/developer/moderator role by having previous and prior experience and intense review by 100chilly with input from other staff. We are to treat everyone the same as well as follow the rules ourselves. If this is not being followed and is brought to 100chilly’s direct attention, He will instantly begin an internal review and take immediate action upon the review results.
+</li>
+      <li>Have fun and relax with others!</li>
+    </ol>
+  
+
+      </div>
+    </div>
+</div>
+</div>
+
+<div class="parallax-container valign-wrapper">
+  <div class="section no-pad-bot">
+    <div class="container">
+      <div class="row center">
+        <h5 class="header col s12 light"></h5>
+      </div>
+    </div>
+  </div>
+  <div class="parallax"><img src="../img/1.jpg" alt="Unsplashed background img 3"></div>
+</div>
+
+<div class="container">
+  <div class="section">
+    <div class="row">
+      <div class="col s12 center">
+        <h3><i class="material-icons green-text">email</i></h3>
+        <h4><b class="white-text">Questions or Concerns?</b></h4>
+        <a href="mailto:support@ivgas.online" class="waves-effect waves-light green darken-2 btn-large"><i class="material-icons left">email</i>Send us an Email</a>
+      </div>
+    </div>
+</div>
+</div>
+
+<div class="parallax-container valign-wrapper">
+  <div class="section no-pad-bot">
+    <div class="container">
+      <div class="row center">
+        <h5 class="header col s12 light"></h5>
+      </div>
+    </div>
+  </div>
+  <div class="parallax"><img src="../img/2.jpg" alt="Unsplashed background img 3"></div>
+</div>
+
+<?php
+$page->showFooter();
+?>
+
+<!--  Scripts-->
+<script type="text/javascript" src='js/jquery/jquery-2.2.4.min.js'></script>
+<script type="text/javascript" src='js/jquery/jquery-ui.min.js'></script>
+<script type="text/javascript" src="js/materialize.js"></script>
+<script type="text/javascript" src="js/init.js"></script>
+
+<script>
+window.addEventListener('HTMLImportsLoaded', function(e) {
+  // all imports loaded
+});
+</script>
+<script>
+  window.addEventListener('WebComponentsReady', function(e) {
+    // imports are loaded and elements have been registered
+  });
+</script>
+
+</body>
+</html>
